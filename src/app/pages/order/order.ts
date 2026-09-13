35009f7ddc89c1edc8786d69be384fc73b21b955
LiubovS <samanthaquick1995@gmail.com>
2026-09-13 22:06:24 +0000

    project

---
+++ src/app/pages/order/order.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Header } from '../../header/header';
+
+@Component({
+  selector: 'app-order',
+  imports: [Header],
+  templateUrl: './order.html',
+  styleUrl: './order.css',
+})
+export class Order {
+
+}
